498bf4d9494935456d5f069613976984bfae5f37
Caleb Frieze <caleb.frieze@proton.me>
2025-12-08 05:45:38 -0700

    update config

--- wezterm.lua
+++ wezterm.lua
@@ -2,9 +2,13 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font_size = 16
+config.font_size = 17
+config.line_height = 1.2
 config.font = wezterm.font("MesloLGS Nerd Font")
 config.color_scheme = "Astrodark (Gogh)"
+
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
 
 local keys = require("keys")
 
